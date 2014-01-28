1566fcda7bbdc0d40a7fd948aaccfb595ac463b8
mala <mala@queue-it.net>
2014-01-28 14:37:43 +0100

    Added build script

---
+++ Build/Build.bat
@@ -0,0 +1,17 @@
+@echo off 
+SET ANT_HOME=%CD%\apache-ant-1.9.3
+
+apache-ant-1.9.3/bin/ant.bat -f "..\QueueIT.Security" clean jar
+IF %ERRORLEVEL% NEQ 0 GOTO Error
+
+xcopy ..\QueueIT.Security\dist\QueueIt.Security.jar /Y
+IF %ERRORLEVEL% NEQ 0 GOTO Error
+
+GOTO End
+
+:Error
+echo ###########################################
+echo # ERROR OCCURED WHILE COMPILING / TESTING #
+echo ###########################################
+
+:End
